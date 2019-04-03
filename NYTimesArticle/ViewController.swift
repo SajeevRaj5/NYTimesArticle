74739df8cfb9cc467a21f69a2e9a135d126d3f87
Sajeev Raj <sajeevraj.1989@gmail.com>
2019-04-03 20:35:16 +0400

    Initial Commit

---
+++ NYTimesArticle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTimesArticle
+//
+//  Created by sajeev Raj on 4/3/19.
+//  Copyright © 2019 Sajeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
